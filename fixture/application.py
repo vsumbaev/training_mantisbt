6fe4e3a9e8f01a01a4a867c507270a02f64191a9
Vsumbaev <vsumbaev@s-terra.com>
2020-12-27 19:51:51 +0300

    soap

--- fixture/application.py
+++ fixture/application.py
@@ -4,6 +4,7 @@ from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
 from fixture.signup import SignupHelper
 from fixture.mail import MailHelper
+from fixture.soap import SoapHelper
 
 
 class Application:
@@ -24,6 +25,7 @@ class Application:
         self.james = JamesHelper(self)
         self.signup = SignupHelper(self)
         self.mail = MailHelper(self)
+        self.soap = SoapHelper(self)
 
 
     def is_valid(self):
--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -12,6 +12,7 @@ def test_signup_new_account(app):
     password = "test"
     app.james.ensure_user_exists(username, password)
     app.signup.new_user(username, email, password)
-    time.sleep(5)
-    assert app.session.is_logget_in_as(username)
-    app.session.logout()
+#    time.sleep(5)
+    assert app.soap.can_login(username, password)
+#    assert app.session.is_logget_in_as(username)
+#    app.session.logout()
